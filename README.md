2585c83a4d5abdabad6f5edacc302fa9a839dd11
meman1188 <meman1188@gmail.com>
2015-01-18 18:38:10 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# service.pftv.updater
+Export favorites from Project Free TV Kodi Addon (plugin.video.pftv) on startup
---
+++ service.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import xbmc
+import sys
+
+if __name__ == '__main__':
+    xbmc.executebuiltin('RunPlugin(plugin://plugin.video.pftv?mode=tv&sitemode=list_tvshows_favor_filtered_export)')
